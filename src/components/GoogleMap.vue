47557ed8297dd16c092737472f367c486a894af9
Visitpattani <visitpattaniapp@gmail.com>
2019-01-03 06:02:30 +0700

    almost final

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -30,14 +30,23 @@
 <script>
 import firebase from 'firebase'
 import {db} from '../firebase'
+let booksRef = db.ref('Location');
 export default {
   name: "GoogleMap",
+  firebase: {
+    posts: booksRef
+  },
   data() {
     return {
       // default to Montreal to keep it simple
       // change this to whatever makes sense
       center: { lat: 7.0071889, lng: 100.4962275 },
-      markers: [],
+      markers: [
+      /*  {
+        position:{
+        lat: 7.0071889, lng: 100.4962275
+      }
+        }*/ ],
       places: [],
       currentPlace: null
     };
@@ -52,17 +61,54 @@ export default {
     setPlace(place) {
       this.currentPlace = place;
     },
-    addMarker() {
-     
-     // if (this.currentPlace) {
-        const marker = {
-          lat:7.0071889,
-          lng: 100.4962275
+    addMarker() {   
+      var i=0;
+      var returnArray=[];
+      var returnArray2=[];
+      var marker=[];
+     /*  const marker = {
+          lat:parseFloat(posts.latt),
+          lng: parseFloat(posts.long)
         };
-        this.markers.push({ position: marker });
-        this.places.push(this.currentPlace);
+        console.log(posts.latt)
+        this.markers.push({ position: marker });*/
+    var dbRef= firebase.database().ref('Location');
+    dbRef.on('value', function(snapshot) {
+   
+    snapshot.forEach(function(child) {  
+    var childs=child.val();
+   var childs1=childs.latt
+   var childs2=childs.long
+   returnArray.push(childs1)
+   returnArray2.push(childs2)
+          console.log(returnArray)
+          console.log(returnArray2)
+       });
+      return returnArray;
+      return returnArray2
+   });
+    
+     // if (this.currentPlace) {
+    //  console.log(lat1)
+     // console.log(lat2)
+   // booksRef.on('value', function(snapshot) {
+   // snapshot.forEach(function(child) {  
+   // var childs=child.val();
+   for(i=0;i<returnArray.length;i++){
+     
+     
+       marker[i] = {
+          lat:returnArray[i],
+          lng: returnArray2[i]
+        };
+        this.markers.push({ position: marker[i] });
+        /*this.places.push(this.currentPlace);
         this.center = marker;
-        this.currentPlace = null;
+        this.currentPlace = null;*/
+   }
+     //  });
+   //});
+      /*
         const marker2 = {
           lat:7.0428779,
           lng: 100.4962275
@@ -70,7 +116,7 @@ export default {
            this.markers.push({ position: marker2 });
         this.places.push(this.currentPlace);
         this.center = marker2;
-        this.currentPlace = null;
+        this.currentPlace = null;*/
     //  }
     
     },
